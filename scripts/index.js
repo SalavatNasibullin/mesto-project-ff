43c69776cc64c3ecf612076ca2c967744a57576e
Salavat Nasibullin <fairslayer@yandex.ru>
2024-12-30 13:46:37 +0500

    your commit message

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,41 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
 
-const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+// DOM узлы
+const placesList = document.querySelector(".places__list"); // Контейнер для вставки карточек
 
-// @todo: DOM узлы
-
-const placesList = document.querySelector('.places__list');
-
-// @todo: Функция создания карточки
-
+// Функция создания карточки
 function createCard(cardContent, onDelete) {
+  // 1. Клонируем темплейт
   const cardElement = cardTemplate.cloneNode(true);
 
-  const cardTitle = cardElement.querySelector('.card__title');
-  const cardImage = cardElement.querySelector('.card__image');
-  const deleteButton = cardElement.querySelector('.card__delete-button');
+  // 2. Выбираем нужные элементы внутри карточки
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-cardTitle.textContent = cardContent.name;
+  // 3. Заполняем карточку данными
+  cardTitle.textContent = cardContent.name;
   cardImage.src = cardContent.link;
   cardImage.alt = cardContent.name;
 
-  deleteButton.addEventListener('click', () => {
+  // 4. Вешаем обработчик для удаления карточки
+  deleteButton.addEventListener("click", () => {
     onDelete(cardElement);
   });
 
   return cardElement;
 }
 
-// @todo: Функция удаления карточки
-
+// Функция удаления карточки
 function deleteCard(cardElement) {
-  if (cardElement.parentNode) {
-    cardElement.parentNode.removeChild(cardElement);
-  }
+  cardElement.remove();
 }
-  
-// @todo: Вывести карточки на страницу
 
-initialCards.forEach((cardData) => {
-  const newCard = createCard(cardData, deleteCard);
+// Вывести карточки на страницу
+initialCards.forEach((item) => {
+  const newCard = createCard(item, deleteCard);
   placesList.append(newCard);
 });
